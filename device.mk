dc9e416f9d19cc98a3e579517c2f6919f2ff3799
NeKit <nekit1000@gmail.com>
2020-07-30 01:12:12 +0200

    device.mk: do not pull in packages already included by halium.mk

--- device.mk
+++ device.mk
@@ -71,23 +71,5 @@ PRODUCT_PACKAGES += \
     mediadrmserver \
     mediaextractor
 
-# Hybris compat libs
-PRODUCT_PACKAGES += \
-    libmedia_compat_layer \
-    libsf_compat_layer \
-    libui_compat_layer
-
-# Droidmedia
-PRODUCT_PACKAGES += \
-    libdroidmedia \
-    minimediaservice \
-    minisfservice \
-    miniafservice
-
-# Ubuntu
-PRODUCT_PACKAGES += \
-    libbiometry_fp_api \
-    libubuntu_application_api
-
 PRODUCT_PACKAGES += \
     sensorservice
