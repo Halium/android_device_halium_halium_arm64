308ad4184d03659a60ddc6960023d795fa15d930
Alexander Martinz <amartinz@shiftphones.com>
2021-11-22 13:45:12 +0100

    halium_arm64: charger: explicitly copy images to product
    Change-Id: Ia03797a58724d99bee72e4e7703723322e7b024e
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- device.mk
+++ device.mk
@@ -14,6 +14,11 @@
 # limitations under the License.
 #
 
+# Charger
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/charger/images/battery_fail.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_fail.png \
+    $(LOCAL_PATH)/charger/images/battery_scale.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_scale.png
+
 # Enable dynamic partition size
 PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
 
