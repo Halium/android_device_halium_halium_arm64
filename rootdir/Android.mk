a958ec7b6639559152bc47b2cb9ea5c5fc62e06e
TheKit <nekit1000@gmail.com>
2022-06-27 14:30:20 +0300

    halium: install an empty file to /system/etc/prop.halium
    It can be used as bind mount target to override property values
    set in any other property files.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -21,6 +21,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := prop.halium
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/prop.halium
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := vndk-detect
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
